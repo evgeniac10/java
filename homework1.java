12cd63d4e9f85b5c70ceed06d637e141664515eb
evgeniac10 <rlagustjd2684@gmail.com>
2023-01-20 21:13:24 +0900

    숙제제출
    제출

---
+++ homework1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; //scanner 클래스 호출;
+
+
+public class homework1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); //스캐너 객체 생성
+	
+		
+		
+		System.out.println ("이름, 도시, 나이, 체중, 독신 여부를 빈칸으로 분리하여 입력하세요.");
+		// 정보를 입력받는다
+		
+		
+		Scanner k = new Scanner(System.in);
+		
+		String str = k.nextLine();
+		
+	}
+		
+}
---
+++ homework2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class homework2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수를 입력하세요:" );
+		int a = sc.nextInt();
+		
+		System.out.print(a + "초는 ");
+		System.out.print(a / 3600 + "시간,");
+		
+		
+	}
+
+}
