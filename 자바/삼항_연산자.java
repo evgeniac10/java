eb6d7aa5bde56363be771ec5c585b05c414d65f9
evgeniac10 <rlagustjd2684@gmail.com>
2023-01-22 16:17:01 +0900

    자바 기초
    기초 공부

---
+++ 자바/삼항_연산자.java
@@ -0,0 +1,14 @@
+package 자바기초;
+
+public class 삼항_연산자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x =  -3;
+		int y = x > 0 ? 2 : -2 ;
+		
+		System.out.println(y);
+	}
+
+}
---
+++ 자바/조건문_스위치.java
@@ -0,0 +1,34 @@
+package 자바기초;
+
+import java.util.Scanner;
+
+public class 조건문_스위치 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 2 ;
+		switch (num) { 	
+			case 0 :
+				System.out.println ( num + " 입니다. ");
+				break ;
+			case 1 : 
+				System.out.println ( num + " 입니다. ");
+				break ;
+			case 2 : 
+				System.out.println ( num + " 입니다. ");
+			case 3 :
+				System.out.println ( num + " 입니다. ");
+				break ;
+			default :
+				System.out.println ( "default 블럭입니다.");
+		}
+		
+		
+			
+			
+			
+		}
+	}
+
+
